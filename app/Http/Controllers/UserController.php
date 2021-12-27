9cbe05a6b311d4c60fd7719c2fe464e08a6d3eb6
AnnishaAprillya <73475038+AnnishaAprillya@users.noreply.github.com>
2021-12-27 15:08:44 +0700

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,22 +5,31 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
+use stdClass;
 
 class UserController extends Controller
 {
     public function register(Request $request){
         $this->validate($request, [
             'username' => 'required|unique:users,username',
-            'password' => 'required'
+            'password' => 'required',
+            'notelpon' => 'required',
+            'alamat' => 'required'
         ]);
         $username = $request->input('username');
         $password = Hash::make($request->input('password'));
+        $notelpon = $request->input('notelpon');
+        $alamat = $request->input('alamat');
 
         $user = User::create([
             'username' => $username,
-            'password' => $password
+            'password' => $password,
+            'notelpon' => $notelpon,
+            'alamat' => $alamat
         ]);
 
-        return response()->json(['message' => 'Pendaftaran pengguna berhasil dilaksanakan']);
+        $api = new \stdClass();
+        $api->data_user = $user;
+        return response()->json($api);
     }
 }
